089f221d1c9cbe21a4e4baf02ba43c00797b265a
Vincent BASTIONI <v.bastioni@gmail.com>
2019-09-23 16:02:24 +0200

    Dungeon building blocks

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::cmp;
+
 use tcod::colors::{self, Color};
 use tcod::console::*;
 
@@ -10,7 +12,51 @@ const MAP_WIDTH: i32 = 80;
 const MAP_HEIGHT: i32 = 40;
 
 const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
-const COLOR_DARK_GROUND: Color = Color { r: 50, g: 50, b: 150 };
+const COLOR_DARK_GROUND: Color = Color {
+    r: 50,
+    g: 50,
+    b: 150,
+};
+
+struct Rect {
+    x1: i32,
+    y1: i32,
+    x2: i32,
+    y2: i32,
+}
+
+impl Rect {
+    pub fn new(x: i32, y: i32, w: i32, h: i32) -> Self {
+        Rect {
+            x1: x,
+            y1: y,
+            x2: x + w,
+            y2: y + h,
+        }
+    }
+}
+
+fn create_room(room: Rect, map: &mut Map) {
+    for x in (room.x1 + 1)..(room.x2) {
+        for y in (room.y1 + 1)..(room.y2) {
+            map[x as usize][y as usize] = Tile::empty();
+        }
+    }
+}
+
+fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
+    for x in cmp::min(x1, x2)..(cmp::max(x1, x2) + 1) {
+        // println!("[{}, {}]", x, y);
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
+    for y in cmp::min(y1, y2)..(cmp::max(y1, y2) + 1) {
+        // println!("[{}, {}]", x, y);
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
 
 #[derive(Debug, Copy, Clone)]
 struct Tile {
@@ -20,11 +66,17 @@ struct Tile {
 
 impl Tile {
     pub fn empty() -> Self {
-        Tile { blocked: false, block_sight: false, }
+        Tile {
+            blocked: false,
+            block_sight: false,
+        }
     }
 
     pub fn wall() -> Self {
-        Tile { blocked: true, block_sight: true, }
+        Tile {
+            blocked: true,
+            block_sight: true,
+        }
     }
 }
 
@@ -38,9 +90,7 @@ struct Object {
 
 impl Object {
     pub fn new(x: i32, y: i32, char: char, color: Color) -> Self {
-        Self {
-            x, y, char, color,
-        }
+        Self { x, y, char, color }
     }
 
     pub fn move_by(&mut self, dx: i32, dy: i32, map: &Map) {
@@ -61,18 +111,17 @@ impl Object {
 
 type Map = Vec<Vec<Tile>>;
 
-// TODO: Tuple struct to define cusom funcs
-// impl Map {
-//     pub fn is_blocked(&self, x: i32, y: i32) -> bool {
-//         self[x as usize][y as usize].blocked
-//     }
-// }
-
 fn make_map() -> Map {
-    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+    // fill the map with "blocked" tiles
+    let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
 
-    map[30][22] = Tile::wall();
-    map[50][22] = Tile::wall();
+    // Create two rooms
+    let room1 = Rect::new(20, 15, 10, 15);
+    let room2 = Rect::new(50, 15, 10, 15);
+    create_room(room1, &mut map);
+    create_room(room2, &mut map);
+
+    create_h_tunnel(25, 55, 23, &mut map);
 
     map
 }
@@ -86,10 +135,15 @@ fn render_all(root: &mut Root, con: &mut Offscreen, objects: &mut [Object], map:
     for y in 0..MAP_HEIGHT {
         for x in 0..MAP_WIDTH {
             let wall = map[x as usize][y as usize].block_sight;
-            con.set_char_background(x, y, match wall {
-                true => COLOR_DARK_WALL,
-                false => COLOR_DARK_GROUND,
-            }, BackgroundFlag::Set);
+            con.set_char_background(
+                x,
+                y,
+                match wall {
+                    true => COLOR_DARK_WALL,
+                    false => COLOR_DARK_GROUND,
+                },
+                BackgroundFlag::Set,
+            );
         }
     }
 
@@ -97,9 +151,11 @@ fn render_all(root: &mut Root, con: &mut Offscreen, objects: &mut [Object], map:
 }
 
 fn main() {
-    let player = Object::new(MAP_WIDTH / 2, MAP_HEIGHT / 2, '@', colors::WHITE);
+    // let player = Object::new(MAP_WIDTH / 2, MAP_HEIGHT / 2, '@', colors::WHITE);
+
+    let player = Object::new(25, 23, '@', colors::WHITE);
+
     let npc = Object::new(MAP_WIDTH / 2 - 5, MAP_HEIGHT / 2, '@', colors::YELLOW);
-    
     // the list of objects with those two
     let mut objects = [player, npc];
 
@@ -126,7 +182,7 @@ fn main() {
         let player = &mut objects[0];
         let exit = handle_keys(&mut root, player, &map);
         if exit {
-            break
+            break;
         }
     }
 }
@@ -141,12 +197,16 @@ fn handle_keys(root: &mut Root, player: &mut Object, map: &Map) -> bool {
         Key { code: Down, .. } => player.move_by(0, 1, map),
         Key { code: Left, .. } => player.move_by(-1, 0, map),
         Key { code: Right, .. } => player.move_by(1, 0, map),
-        Key { code: Enter, alt: true, .. } => {
+        Key {
+            code: Enter,
+            alt: true,
+            ..
+        } => {
             let fullscreen = root.is_fullscreen();
             root.set_fullscreen(!fullscreen);
-        },
+        }
         Key { code: Escape, .. } => return true,
-        _ => {},
+        _ => {}
     }
 
     false
